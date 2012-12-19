4e0b48e83aac88895e2fda810432e2d4a0f9f3c7
patamau <patamau@gmail.com>
2012-12-19 17:15:17 +0000

    added ai movement change default character weapon BAZOOKA D:

--- src/main/java/it/patamau/ld25/entities/Character.java
+++ src/main/java/it/patamau/ld25/entities/Character.java
@@ -16,7 +16,7 @@ public class Character extends Entity {
 		size = 24f;
 		sprite = Sprite.getSprite("c");
 		hitpoints = 100;
-		weapon = Weapon.PISTOL.clone();
+		weapon = Weapon.BAZOOKA.clone();
 	}
 
 	public void dealDamage(Bullet b) {
